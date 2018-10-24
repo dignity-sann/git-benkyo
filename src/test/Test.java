ae21822b360cc8d1e1113851543a94245b9c0bfa
dignity-sann <sunday.ktkr.dict@gmail.com>
2018-10-24 14:27:03 +0900

    dev1 initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * test
+ * @author dignity-sann
+ */
+class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
